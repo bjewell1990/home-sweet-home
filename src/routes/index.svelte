940863feb8b01f1e035f000d57e3382c8174330e
bjewell1990 <rbertram8@gmail.com>
2022-05-17 21:58:18 -0500

    remove default text

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,5 +5,3 @@
 
 <Header /><Imagine src="/family.jpeg" />
 <Imagine src="/family.jpeg" /><Imagine src="/family.jpeg" /><Imagine src="/family.jpeg" />
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
