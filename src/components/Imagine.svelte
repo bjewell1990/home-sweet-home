321e3aa2482c539b60aa8080020ce16c1563c56f
bjewell1990 <rbertram8@gmail.com>
2022-05-25 21:01:37 -0500

    add title

--- src/components/Imagine.svelte
+++ src/components/Imagine.svelte
@@ -1,16 +1,27 @@
 <script>
 	export let src;
+	export let title = '';
 </script>
 
-<div style={`background-image:url("${src}")`} class="derp" />
+<div class="wrap">
+	<div style={`background-image:url("${src}")`} class="derp" />
+	<p class="title">{title}</p>
+</div>
 
 <style>
+	.wrap {
+		margin: 20px;
+	}
+	.title {
+		text-align: center;
+		color: #555;
+	}
 	.derp {
 		width: 250px;
 		height: 400px;
 		border: 4px solid purple;
 		border-radius: 8px;
-		margin: 20px;
+
 		background-position: center;
 		background-size: cover;
 	}
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,12 +3,18 @@
 	import Imagine from '../components/Imagine.svelte';
 
 	let images = [
-		{ src: '/canal-walk.jpg' },
-		{ src: '/colorful-markers.jpg' },
-		{ src: '/guitars.jpg' },
-		{ src: '/paints.jpg' },
-		{ src: '/tye-dye.jpg' },
-		{ src: '/zebra-with-robbie.jpg' }
+		{ src: '/canal-walk.jpg', title: 'A beautiful day on the canal' },
+		{ src: '/colorful-markers.jpg', title: 'Colorful Markers' },
+		{ src: '/guitars.jpg', title: 'Colorful Guitars' },
+		{ src: '/paints.jpg', title: 'Painting day' },
+		{ src: '/tye-dye.jpg', title: 'Tye-Dye high dye' },
+		{ src: '/zebra-with-robbie.jpg', title: 'Zebra with Robbie' },
+		{ src: '/Buttons.jpeg', title: 'Colorful Buttons' },
+		{ src: '/Fruit.jpeg', title: 'Colorful Fruit' },
+		{ src: '/Colorful-wow.jpeg', title: 'Colorful wow' },
+		{ src: '/Ice-Cream.jpeg', title: 'Ice Cream Shop' },
+		{ src: '/Im on the boat.jpeg', title: "I'm on a boat!" },
+		{ src: '/Umbrellas.jpeg', title: 'Colorful Umbrellas' }
 	];
 </script>
 
@@ -16,7 +22,7 @@
 
 <div class="format">
 	{#each images as image}
-		<Imagine src={image.src} />
+		<Imagine src={image.src} title={image.title} />
 	{/each}
 </div>
 
